7d997ea25f1f5d10c016a946d444fb8b4cce4e92
Yassamine-sahl <85345273+Yassamine-sahl@users.noreply.github.com>
2021-06-12 22:21:09 +0100

    commit

--- YASSMINE/migrations/20210612205947-create-user.js
+++ YASSMINE/migrations/20210612205947-create-user.js
@@ -9,10 +9,14 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       username: {
-        type: Sequelize.STRING
+        type : Sequelize.STRING,
+        unique : true,
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type : Sequelize.STRING,
+        unique : true,
+        allowNull: false
       },
       password: {
         type: Sequelize.STRING
