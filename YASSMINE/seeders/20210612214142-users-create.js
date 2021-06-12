b9ba72761026427738243225abdefe168dcbb16d
Yassamine-sahl <85345273+Yassamine-sahl@users.noreply.github.com>
2021-06-12 23:05:00 +0100

    commit

--- YASSMINE/seeders/20210612214142-users-create.js
+++ YASSMINE/seeders/20210612214142-users-create.js
@@ -1,4 +1,17 @@
 'use strict';
+const fake = require("faker")
+
+const users=[...Array(20)].map((user)=>({
+
+  username :fake.internet.userName(),
+  email:fake.internet.email(),
+  password :fake.internet.password(8),
+  role :fake.helpers.randomize(['author','admin','guest']),
+  createdAt :new Date(),
+  updatedAt :new Date()
+  
+})
+)
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
