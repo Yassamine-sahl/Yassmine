fd329de18f9bc8d05ac496204a59fba2d3ae0c67
Yassamine-sahl <85345273+Yassamine-sahl@users.noreply.github.com>
2021-06-13 18:52:05 +0100

    commit

---
+++ YASSMINE/repositories/comments.js
@@ -0,0 +1,86 @@
+const { Comment } = require('../models')
+
+const { Article } = require('../models') 
+
+var sequelize = require('sequelize');
+
+ module.exports = {
+   getAllComments() {
+     return Article.findAll({
+      group : ['Article.id'],
+      attributes: ['title', [sequelize.fn('COUNT', 'Comments.id'), 'NbrComments']],
+      include: { model: Comment,attributes: []}
+    });
+   },
+
+   countComments() {
+    return Article.findAll({
+     group : ['Article.id'],
+     attributes: ['id','title','content','createdAt', [sequelize.fn('COUNT', 'Comments.id'), 'NbrComments']],
+     include: { model: Comment,attributes: []}
+   });
+  },
+
+   getComments(offset = 0, limit = 10){
+    return  Comment.findAll({ offset: offset, limit: limit })
+  } ,
+
+  async countArticle(ida){
+    return await Comment.count({
+      where:  {
+        id:ida 
+      }
+    });
+  },
+
+   getComments(id) {
+    var x= Comment.findAll({
+      where: {       
+          id: id
+      }
+    });
+    return x
+    },
+    
+    getArticleComments(id) {
+      var x= Comment.findAll({
+        where: {       
+          ArticleId: id
+        }
+      });
+      return x
+      },
+
+    getPostComments(id) {
+      var x= Comment.findAll({
+        where: {       
+          ArticleId: id
+        }
+      });
+      return x
+      },
+
+
+    async addComments(Comments) { 
+      await Comment.create(Tags);
+    },
+      
+
+    async updateComments(id,Comments) { 
+    await Comment.update(Tags, {
+      where: {
+        id: id
+      }
+    });
+    },
+
+
+    async deleteComments(id) { 
+    await Comment.destroy({
+      where: {
+        id: id
+      }
+    });
+  },
+   // D'autres méthodes jugées utiles
+ }
